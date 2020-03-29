e301107f2014ee2630f4a45da84187db1674ef4a
Jonathan Funk <jonfunk88@hotmail.com>
2020-03-29 15:24:22 -0700

    added expand popup button

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -6,6 +6,7 @@ import RecipeContentPlaceholder from './RecipeContentPlaceholder';
 import Error from './Error';
 import Popup from 'reactjs-popup';
 import singleMockData from './../singleMockData';
+import expand from './../images/expand.svg';
 
 const RecipeCard = ({ id, title, image, missingIngredient = null }) => {
   const [open, setOpen] = useState(false);
@@ -44,16 +45,21 @@ const RecipeCard = ({ id, title, image, missingIngredient = null }) => {
   return (
     <Fragment>
       <div className="pb-2/3 relative bg-gray-900 rounded overflow-hidden">
+        <div className="absolute z-20 w-16 h-16 flex justify-center items-center right-0">
+          <div
+            className="w-10 h-10 bg-orange-500 rounded-full leading-10 text-white hover:bg-orange-400 focus:outline-none flex items-center justify-center cursor-pointer"
+            onClick={openPopup}
+          >
+            <img className="h-6 w-6" src={expand} alt="Expand Icon" />
+          </div>
+        </div>
         <img
           className="absolute top-0 left-0 w-full h-full object-cover z-0 opacity-75"
           src={image}
           alt={title}
         />
         <div className="absolute top-0 left-0 w-full h-full z-10 p-4 flex flex-col justify-end">
-          <h3
-            className="relative text-white text-xl font-medium text-center mb-2 hover:text-gray-200 cursor-pointer"
-            onClick={openPopup}
-          >
+          <h3 className="relative text-white text-xl font-medium text-center mb-2 hover:text-gray-200 cursor-pointer">
             {title}
           </h3>
           <div className="text-sm text-center text-gray-100 h-5">
