0562be30b406262e78ab7c44835efe55eaba8552
Jonathan Funk <jonfunk88@hotmail.com>
2020-03-29 19:08:01 -0700

    Updates

---
+++ precache-manifest.6a853300f2f910a245c714c869a3ea5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499b50fd1a7762d8836d9db95d394091",
+    "url": "/whats-in-your-kitchen/index.html"
+  },
+  {
+    "revision": "a44d74dd2f8ccccb687b",
+    "url": "/whats-in-your-kitchen/static/css/main.f26bca05.chunk.css"
+  },
+  {
+    "revision": "dc2037481ce4d710c13a",
+    "url": "/whats-in-your-kitchen/static/js/2.fe3292f9.chunk.js"
+  },
+  {
+    "revision": "d9dd48947f9419eda5bc1b1f25880918",
+    "url": "/whats-in-your-kitchen/static/js/2.fe3292f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44d74dd2f8ccccb687b",
+    "url": "/whats-in-your-kitchen/static/js/main.0c14fdfc.chunk.js"
+  },
+  {
+    "revision": "9df149fb8326ca864f2a",
+    "url": "/whats-in-your-kitchen/static/js/runtime-main.b4259267.js"
+  },
+  {
+    "revision": "08f88f57a9b8eb9ee81c3b408076ea64",
+    "url": "/whats-in-your-kitchen/static/media/expand.08f88f57.svg"
+  },
+  {
+    "revision": "38f88846da56639cff9c50c3ce9a4603",
+    "url": "/whats-in-your-kitchen/static/media/fridge.38f88846.svg"
+  },
+  {
+    "revision": "892168a66aef31ca2486fb404d449625",
+    "url": "/whats-in-your-kitchen/static/media/wiyk-bg.892168a6.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/whats-in-your-kitchen/precache-manifest.6a853300f2f910a245c714c869a3ea5d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whats-in-your-kitchen/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
